57ba9acf1de06e477677077d1f69f8b802cd293d
Andres Bolivar <andresbolivar07@hotmail.com>
2023-08-08 18:22:15 -0500

    feat: functions for data studio ghibli

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
+
+const prinData = (movies) => {
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'container');
+
+    document.getElementById('root').appendChild(newDiv);
+
+    movies.films.forEach(element => {
+        const template = `
+        <div class="card">
+            <div class="title">${element.title}</div>
+            <div class="photo">
+                <img src="${element.poster}">
+            </div>`;
+            newDiv.innerHTML += template;
+    })
+}
+prinData(data);
 
 console.log(example, data);
