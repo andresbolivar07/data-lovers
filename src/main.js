a32e91a045c8bce6d7c99cd2a748a10037eff75a
Andres Bolivar <andresbolivar07@hotmail.com>
2023-08-17 23:43:35 -0500

    feat: create review button

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { filterData, sortData } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 const filmsArray = data['films'];
-
+const reviewArray = data ['Review']
 
 const printData = (movies) => {
 
@@ -28,10 +28,40 @@ const printData = (movies) => {
     })
 }
 
+const printReview = (review) => {
+
+    if (document.getElementById('ghibliReview')){
+        const cleanContainer = document.getElementById('container');
+        document.getElementById('root').removeChild(cleanContainer);
+    }
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'container');
+
+    document.getElementById('root').appendChild(newDiv);
+
+    review.forEach(element => {
+        const template = `
+        <div class="card">
+            <div class="title">${element.name}</div>
+            <div class="release_date">${element.description}</div>
+            <div class="photo">
+                <img src="${element.img}">
+                <img src="${element.img2}">
+            </div> `;
+            newDiv.innerHTML += template;
+    })
+}
+
 const loadAll = document.getElementById("showAll");
 loadAll.addEventListener('click', () => {
     /* Llamada de función showData para mostrar peliculas*/    
     printData(filmsArray);
+});
+
+const reviewGhibli = document.getElementById("review");
+reviewGhibli.addEventListener('click', () => {
+       
+    printReview(reviewArray);
 });
 
 const searchBtn = document.getElementById("submitBtn");
@@ -41,19 +71,16 @@ searchBtn.addEventListener('click', () => {
     printData(filterFunction);
 });
 
-
 const upwardBtn = document.getElementById("upward");
 upwardBtn.addEventListener('click', () => {
         
     printData(sortData(data, 'title'));
-    
 });
 
 const fallingBtn = document.getElementById("falling");
 fallingBtn.addEventListener('click', () => {
         
     printData(sortData(data, 'title').reverse());
-    
 });
 
 
