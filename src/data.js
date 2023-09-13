e0500d67820a8e3549c10734fce6e29d311e486b
Andres Bolivar <andresbolivar07@hotmail.com>
2023-09-13 16:23:54 -0500

    feat: change in result variable

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 
 export const filterData = (search, data, filterBy) => {
   if (filterBy === 'title') {
-    let result = data.filter(data => data.title.toLowerCase().includes(search.toLowerCase()));
+    const result = data.filter(data => data.title.toLowerCase().includes(search.toLowerCase()));
     return result;
   }
 };
@@ -10,7 +10,7 @@ export const sortData = (data) => {
   const arrayFilms = Array.from(data['films']);
   // Array.from permite hacer copia de un array
   
-  let result = arrayFilms.sort(function(a,b) {
+  const result = arrayFilms.sort(function(a,b) {
     if (a.title > b.title) {
       return 1;
     }
